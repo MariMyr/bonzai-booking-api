f119ab8e0dd6a0c1592d3b3e42dcf4b1ae7989e2
Carolina-Cardenas <carolina.beatriz.cardenas@gmail.com>
2025-09-15 20:00:03 +0200

    started with getBooking function

---
+++ functions/getBooking/index.mjs
@@ -0,0 +1,26 @@
+import { GetItemCommand } from "@aws-sdk/client-dynamodb";
+import { client } from "../../services/db.mjs";
+import { sendResponse } from "../../utils/responses/index.mjs";
+
+export const handler = async (event) => {
+  try {
+    const bookingId = event.pathParameters?.id;
+
+    if (!bookingId) {
+      return sendResponse(400, { error: "Missing bookingId" });
+    }
+    const command = new GetItemCommand({
+      TableName: "BonzaiBookings",
+      Key: {
+        id: { S: bookingId },
+      },
+    });
+
+    const result = await client.send(command);
+
+    if (!result.Item) {
+      return sendResponse(404, { error: "Booking not found" });
+    }
+
+   
+};
